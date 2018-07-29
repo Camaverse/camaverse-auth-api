f1d69ee2f8378183341e1b3156ffa58d5ee7ead5
larrylsharpe <admin@capital-mark.com>
2018-07-29 08:56:42 -0400

    - updated seed to get broadcaster usernames, tags and topics from mock json - added ability to add new tag if approvalDate is less than 4 weeks ago

--- api/1.0/seed/seed.js
+++ api/1.0/seed/seed.js
@@ -6,7 +6,13 @@ const UserModel = require("../resources/users/users.model");
 const BroadcasterModel = require("../resources/broadcasters/broadcasters.model")
 const ChatroomModel = require("../resources/chatrooms/chatrooms.model")
 const SystemModel = require("../resources/system/system.model");
-const org_tags = ["abc","def","hij","klm","nop","qrs","tuv","wxy","z"];
+
+const randoms = require('./randomGenerators');
+
+const girlNames = require('./girlNames.json');
+const tags = require('./tagsNSFW.json');
+const topics = require('./randonSentencesNSFW.json');
+
 const counts = {
     user: 10,
     admin: 3,
@@ -48,17 +54,20 @@ const createMethods = {
     onlineBroadcaster (usr, type, methodType, num, offset, resolve) {
         usr.then((user) => {
             console.log(`${user.username} user saved.`)
+
+            const topic = topics[randoms.number(topics.length)];
+
             let broadcaster = new BroadcasterModel(new Broadcaster(user.username, true))
             let chatroom = new ChatroomModel({
                 slug: broadcaster.slug,
                 broadcasterID: broadcaster._id,
                 socket: 'crecercercercre',
                 username: broadcaster.username,
-                topic: 'Chatroom Topic Goes Here',
+                topic,
                 images: broadcaster.images,
                 tags: broadcaster.tags,
-                viewers: rando(2000),
-                xp: rando()
+                viewers: randoms.number(2000,0),
+                xp: randoms.number()
             })
 
             // add chatroom broadcaster is in to broadcaster
@@ -101,21 +110,9 @@ const createMethods = {
         })
     }
 }
-
 createMethods.admin = createMethods.user;
 
-let tags = org_tags.join(',').split(',');
-
 console.log(process.env.DB_CONNECT);
-
-for (let i = 0; i < 10; i ++) {
-    for (let tag in org_tags) {
-        let newTag = org_tags[tag] + i
-        tags.push(newTag)
-    }
-}
-
-const rando = (max = 100000) => Math.floor(Math.random() * max) + 1
 
 class User {
     constructor (
@@ -141,7 +138,7 @@ class Broadcaster {
         isOnline = false
     ) {
 
-        const approvalDate = this.randomDate(new Date(2012, 0, 1), new Date());
+        const approvalDate = randoms.date(new Date(2012, 0, 1), new Date());
         const approvalTime = approvalDate.getTime();
         const newCutOff = Date.now() - 12096e5 - 12096e5;
         const isNew = newCutOff < approvalDate;
@@ -154,38 +151,24 @@ class Broadcaster {
         if (isNew) this.tags.push('new');
         this.approved = approvalDate;
         this.images = {
-            broadcasterGrid: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-" + rando(19) +  ".jpg"
+            broadcasterGrid: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/70390/show-" + randoms.number(19, 1) +  ".jpg"
         }
-        this.xp = rando()
-    }
-
-    randomDate(start, end) {
-        return new Date(start.getTime() + Math.random() * (end.getTime() - start.getTime()));
+        this.xp = randoms.number()
     }
 
     createTags () {
         let tag_list = [];
-        const _loop = this.randomIndex(8, 1);
+        const _loop = randoms.number(5, 1);
         for (let i = 0; i < _loop; i++){
-            const _index = this.randomIndex(tags.length, 0);
+            const _index = randoms.number(tags.length, 0);
             let tag = tags[_index]
             if (tag_list.indexOf(tag) === -1) tag_list.push(tag);
         }
         return tag_list;
     }
-
-    randomIndex (max, min) {
-        return (Math.floor(Math.random() * max) + min);
-    }
 }
 
 mongoose.connect(process.env.DB_CONNECT);
-
-const objectsave = (err) => {
-    if (err) {
-        console.log(err)
-    }
-}
 
 const recNext = (resolve, type, methodType, offset) => {
     if (counts[methodType]) {
@@ -198,7 +181,18 @@ const recordsLoop = (resolve, type, methodType, offset) => {
     if (counts[methodType]) {
         const num = totals[methodType] - counts[methodType];
         const numTtl = num + offset;
-        const usr = new UserModel(new User(`${type} ${numTtl}`, 'royalties', [type])).save();
+
+        let username;
+
+        if (type === 'broadcaster') {
+            usernameIndex = randoms.number(girlNames.length);
+            username = girlNames[usernameIndex];
+            girlNames.splice(usernameIndex, 1);
+        } else {
+            username = `${type} ${numTtl}`;
+        }
+
+        const usr = new UserModel(new User(username, 'royalties', [type])).save();
         createMethods[methodType](usr, type, methodType, num, offset, resolve)
     } else {
         console.log('complete')
