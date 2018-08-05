5e3ea33a5b8780c7e121feed394d28330c529485
larrylsharpe <admin@capital-mark.com>
2018-08-05 11:45:17 -0400

    removed unnecessary user routes, refactored some user functions

--- api/1.0/resources/users/users.controller.js
+++ api/1.0/resources/users/users.controller.js
@@ -4,6 +4,27 @@ const SystemModel = require('../system/system.model')
 const slugify = require('slugify')
 const {success, error} = require('../../helpers/ApiResponse');
 
+const checkOTP = (user, otpHeader) => {
+
+    return new Promise((resolve, reject) => {
+        if (!user.twofactor.secret){
+            resolve(user)
+        } else {
+            if (!otpHeader){
+                reject(new Error('Please enter otp to continue'))
+            }
+        }
+    })
+}
+const getCoins = (req, res) => {
+    let usernameLower = (req && req.query && req.query.username) ? req.query.username :
+        (req && req.body && req.body.username) ? req.body.username : null;
+    usernameLower = usernameLower.toLowerCase()
+
+    User.findOne({usernameLower})
+        .then((user) => respond(null, user.coins.balance, res))
+        .catch((err) => respond(err, null, res))
+}
 const getJWTAndUser = (user) => {
     return new Promise(
         (resolve, reject) => {
@@ -14,7 +35,70 @@ const getJWTAndUser = (user) => {
         }
     )
 }
+const init = (req, res, user, cb) => {
+    if (!user || !user.username) {
+        SystemModel.find({})
+            .then((rec) => {
+                if (!rec.length){
+                    cb('System Not Found', null);
+                } else {
+                    rec = rec[0]
+                    const guestNum = rec.guestNo;
+                    const username = 'Guest ' + guestNum;
+                    const slug = slugify(username, {lower: true});
 
+                    let guest = {
+                        // ip: socket.handshake.address,
+                        // isLoggedIn: false,
+                        // roles: ['guest'],
+                        slug,
+                        // status: 'online',
+                        username
+                    }
+                    if (cb) cb(null, guest);
+                    rec.guestNo++
+                    rec.save();
+                }
+            })
+            .catch(cb)
+    } else {
+        const usernameLower = user.username.toLowerCase().trim()
+        User.findOne({usernameLower})
+            .then((user) => {
+                return respond(null, user.loginInfo, cb)
+            })
+            .catch((err) => respond(err, null, cb))
+    }
+}
+const login = (req, res) => {
+    if (validate.login(req.body)) {
+        const usernameLower = req.body.username.toLowerCase().trim()
+        const password = req.body.password
+        let qry = { usernameLower }
+        if (req.body.roles) {
+            qry.roles = req.body.roles;
+        }
+
+        User.findOne(qry)
+            .then((user) => validate.password(user, password))
+            .then(updateUserOnLogin)
+            .then(getJWTAndUser)
+            .then((user) => respond(null, user, res))
+            .catch((err) => respond(err, null, res))
+    }
+}
+const logout = (req, res) => {
+    let usernameLower = (req && req.query && req.query.username) ? req.query.username :
+        (req && req.body && req.body.username) ? req.body.username : null;
+    usernameLower = usernameLower.toLowerCase().trim()
+    const qry = {usernameLower};
+    const update = {isLoggedIn: false, status: 'offline', '$push': { logouts: Date.now() }}
+    const options = { new: true };
+
+    User.findOneAndUpdate(qry, update, options)
+        .then((user) => respond(null, user, res))
+        .catch((err) => respond(err, null, res))
+}
 const respond = (err, docs, res) => {
 
     let response;
@@ -33,14 +117,33 @@ const respond = (err, docs, res) => {
         console.log(response)
     }
 }
+const signup = (req, res) => {
+    if (!req.body.username || !req.body.password || !req.body.email) {
+        res.json({success: false, msg: 'Please pass username and email and password.'});
+    } else {
+        var newUser = new User({
+            email: req.body.email,
+            username: req.body.username,
+            password: req.body.password
+        });
 
+        if (req.body.roles) newUser.roles = [req.body.roles];
+
+        // save the user
+        newUser.save(function(err) {
+            if (err) {
+                return res.status(500).json({success: false, msg: err});
+            }
+            res.json({success: true, msg: 'Successful created new user.'});
+        });
+    }
+}
 const updateUserOnLogin = (user) => {
     user.status = 'online';
     user.isLoggedIn = true;
     user.logins.push(Date.now());
     return user.save()
 }
-
 const validate = {
     login (input){
         return (input && input.username && input.password)
@@ -56,152 +159,11 @@ const validate = {
     }
 }
 
-const checkOTP = (user, otpHeader) => {
-    return new Promise((resolve, reject) => {
-        if (!user.twofactor.secret){
-            resolve(user)
-        } else {
-            if (!otpHeader){
-                reject(new Error('Please enter otp to continue'))
-            }
-        }
-    })
-}
-
 module.exports = {
-    create (req, res) {
-        let usr = new User(req.body)
-        usr.save((err, docs) =>
-            respond(err, (docs && docs.createInfo) ? docs.createInfo : null, res));
-    },
-    getCoins (req, res) {
-        let usernameLower = (req && req.query && req.query.username) ? req.query.username :
-            (req && req.body && req.body.username) ? req.body.username : null;
-        usernameLower = usernameLower.toLowerCase()
-
-        User.findOne({usernameLower})
-            .then((user) => respond(null, user.coins.balance, res))
-            .catch((err) => respond(err, null, res))
-    },
-    guestInit (cb) {
-        SystemModel.find({}, (err, rec) => {
-            if (!err) {
-                if (rec.length) {
-                    rec = rec[0]
-                    const guestNum = rec.guestNo;
-                    const username = 'Guest ' + guestNum;
-                    const slug = slugify(username, {lower: true});
-
-                    let guest = {
-                        // ip: socket.handshake.address,
-                        // isLoggedIn: false,
-                        // roles: ['guest'],
-                        slug,
-                        // status: 'online',
-                        username
-                    }
-                    if (cb) cb(null, guest);
-                    rec.guestNo++
-                    rec.save();
-                } else {
-                    cb('System Not Found', null);
-                }
-            }
-
-        });
-    },
-    findAll (req, res) {
-       User.find({})
-           .then((docs) => respond(null, docs, res))
-           .catch((err) => respond(err, docs, res))
-    },
-    findBroadcasters (req, res) {
-       User.find({roles: "broadcaster"})
-           .then((docs) => respond(null, docs, res))
-           .catch((err) => respond(err, docs, res))
-    },
-    findUsers (req, res) {
-       User.find({roles: "user"})
-           .then((docs) => respond(null, docs, res))
-           .catch((err) => respond(err, docs, res))
-    },
-
-    login (req, res) {
-        if (validate.login(req.body)) {
-            const usernameLower = req.body.username.toLowerCase().trim()
-            const password = req.body.password
-            let qry = { usernameLower }
-            if (req.body.roles) {
-                qry.roles = req.body.roles;
-            }
-
-            User.findOne(qry)
-                .then((user) => validate.password(user, password))
-                .then(updateUserOnLogin)
-                .then(getJWTAndUser)
-                .then((user) => respond(null, user, res))
-                .catch((err) => respond(err, null, res))
-        }
-    },
-    logout (req, res) {
-        let usernameLower = (req && req.query && req.query.username) ? req.query.username :
-            (req && req.body && req.body.username) ? req.body.username : null;
-        usernameLower = usernameLower.toLowerCase().trim()
-        const qry = {usernameLower};
-        const update = {isLoggedIn: false, status: 'offline', '$push': { logouts: Date.now() }}
-        const options = { new: true };
-
-        User.findOneAndUpdate(qry, update, options)
-            .then((user) => respond(null, user, res))
-            .catch((err) => respond(err, null, res))
-    },
-    signup (req, res) {
-        if (!req.body.username || !req.body.password || !req.body.email) {
-            res.json({success: false, msg: 'Please pass username and email and password.'});
-        } else {
-            var newUser = new User({
-                email: req.body.email,
-                username: req.body.username,
-                password: req.body.password
-            });
-
-            if (req.body.roles) newUser.roles = [req.body.roles];
-
-            // save the user
-            newUser.save(function(err) {
-                if (err) {
-                    return res.status(500).json({success: false, msg: err});
-                }
-                res.json({success: true, msg: 'Successful created new user.'});
-            });
-        }
-    },
-
-    init (user, cb) {
-        if (!user || !user.username) respond({err: 'Invalid request'}, null, cb)
-        else {
-            const usernameLower = user.username.toLowerCase().trim()
-            User.findOne({usernameLower})
-                .then((user) => {
-                    return respond(null, user.loginInfo, cb)
-                })
-                .catch((err) => respond(err, null, cb))
-        }
-    },
-
-    logout (req, res) {
-        const qry = {username : req.params.username};
-        const update = {isLoggedIn: false, status: 'offline', '$push': { logouts: Date.now() }}
-        const options = { new: true };
-
-        User.findOneAndUpdate(qry, update, options, (err) => {
-                if (err) {
-                    return res.status(500).json({success: false, msg: err});
-                }else {
-                    return res.status(200).json({success: true });
-            }
-        })
-    }
-
+    getCoins,
+    init,
+    login,
+    logout,
+    signup
 }
 
--- api/1.0/resources/users/users.routes.js
+++ api/1.0/resources/users/users.routes.js
@@ -3,25 +3,18 @@ const router = express.Router();
 const controller = require('./users.controller');
 
 exports = module.exports = function(io) {
-    router.post('/', controller.create)
-    router.post('/login', controller.login)
-    router.post('/logout', controller.logout);
+
+    router.route('/')
+        .get(controller.logout)
+        .post(controller.logout);
+    router.get('/logout/:username', controller.logout);
+    router.post('/login', controller.login);
     router.post('/signup', controller.signup);
-    router.get('/logout/:username', controller.logout)
-    router.get('/', controller.findAll);
-    router.get('/broadcasters', controller.findBroadcasters);
-    router.get('/logout', controller.logout);
-    router.get('/users', controller.findUsers);
 
     io.on('connection', function (socket) {
-        socket.on('/guests/init', controller.guestInit)
-        socket.on('/post/users', controller.create)
-        socket.on('/users', controller.findAll)
-        socket.on('/users/broadcasters', controller.findBroadcasters)
         socket.on('/users/init', controller.init)
         socket.on('/users/login', controller.login)
         socket.on('/users/logout', controller.logout)
-        socket.on('/users/users', controller.findUsers)
         socket.on('/users/coins', controller.getCoins)
     })
 
